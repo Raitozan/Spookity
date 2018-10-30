2fba831701cf244637e04a71cda55b6d47e1a0c6
DrHurley <hurley69@outlook.com>
2018-10-30 17:11:48 +0100

    deplacement + camera

---
+++ Assets/Scripts/Move_Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Camera : MonoBehaviour
+{
+
+    private float mouseX;
+    public Transform playerCam, character, centerPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        mouseX += Input.GetAxis("Mouse X");
+        playerCam.LookAt(centerPoint);
+        centerPoint.localRotation = Quaternion.Euler(0, mouseX * 10, 0);
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private float speed = 5.0f;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+       
+        transform.Translate(x * speed, 0, z * speed);
+    }
+}
