4b6337a664cd589b62ee0d09bcbf4a7072a588de
Xenoxygene <Xenoxygene@users.noreply.github.com>
2018-11-02 11:50:49 +0100

    Ajout touches
    Ajout du support manette et touches clavier + gravité au cas où

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,9 +14,59 @@ public class PlayerController : MonoBehaviour {
 
     void Update()
     {
-		var x = Input.GetAxis("Horizontal");
+        Move();
+        LeftArm();
+        RightArm();
+        LeftLeg();
+        RightLeg();
+        Head();
+    }
+
+    private void Move()
+    {
+        var x = Input.GetAxis("Horizontal");
         var z = Input.GetAxis("Vertical");
-       
+
         transform.Translate(x * speed * Time.deltaTime, 0, z * speed * Time.deltaTime);
+    }
+
+    private void LeftArm()
+    {
+        if (Input.GetButtonDown("LeftArm"))
+        {
+            Debug.Log("Lancer bras gauche");
+        }
+    }
+
+    private void RightArm()
+    {
+        if (Input.GetButtonDown("RightArm"))
+        {
+            Debug.Log("Lancer bras droit");
+        }
+    }
+
+    private void LeftLeg()
+    {
+        if (Input.GetAxis("LeftLeg") >= 0.75)
+        {
+            Debug.Log("Lancer jambe gauche");
+        }
+    }
+
+    private void RightLeg()
+    {
+        if (Input.GetAxis("RightLeg") >= 0.75)
+        {
+            Debug.Log("Lancer jambe droite");
+        }
+    }
+
+    private void Head()
+    {
+        if (Input.GetButtonDown("Head"))
+        {
+            Debug.Log("Lancer tête");
+        }
     }
 }
