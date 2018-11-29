4707a1deab06e5f93487a5d4f140299eb4190760
Xenoxygene <Xenoxygene@users.noreply.github.com>
2018-11-29 13:39:48 +0100

    2 joueurs
    Contrôles + Caméra

--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController2 : MonoBehaviour {
 
     private float speed = 15.0f;
 
     void Start()
     {
-      
+
     }
 
 
@@ -24,50 +24,49 @@ public class PlayerController : MonoBehaviour {
 
     private void Move()
     {
-        var x = Input.GetAxis("Horizontal");
-        var z = Input.GetAxis("Vertical");
+        var x = Input.GetAxis("Horizontal2");
+        var z = Input.GetAxis("Vertical2");
 
         transform.Translate(x * speed * Time.deltaTime, 0, z * speed * Time.deltaTime);
     }
 
     private void LeftArm()
     {
-        if (Input.GetButtonDown("LeftArm"))
+        if (Input.GetButtonDown("LeftArm2"))
         {
-            Debug.Log("Lancer bras gauche");
+            Debug.Log("Lancer bras gauche 2");
         }
     }
 
     private void RightArm()
     {
-        if (Input.GetButtonDown("RightArm"))
+        if (Input.GetButtonDown("RightArm2"))
         {
-            Debug.Log("Lancer bras droit");
+            Debug.Log("Lancer bras droit 2");
         }
     }
 
     private void LeftLeg()
     {
-        if (Input.GetAxis("LeftLeg") >= 0.75)
+        if (Input.GetAxis("LeftLeg2") >= 0.75)
         {
-            Debug.Log("Lancer jambe gauche");
+            Debug.Log("Lancer jambe gauche 2");
         }
     }
 
     private void RightLeg()
     {
-        if (Input.GetAxis("RightLeg") >= 0.75)
+        if (Input.GetAxis("RightLeg2") >= 0.75)
         {
-            Debug.Log("Lancer jambe droite");
+            Debug.Log("Lancer jambe droite 2");
         }
     }
 
     private void Head()
     {
-        if (Input.GetButtonDown("Head"))
+        if (Input.GetButtonDown("Head2"))
         {
-            Debug.Log("Lancer tête");
+            Debug.Log("Lancer tête 2");
         }
-        
     }
 }
