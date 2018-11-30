a2907dd157d810ce1677f85e9f1dbf621afe4fce
Nathan Desages <nathan.desages@gmail.com>
2018-11-30 13:05:00 +0100

    Skybox and light tweak

--- Assets/Scripts/FlipSphere.cs
+++ Assets/Scripts/FlipSphere.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class FlipSphere : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Start ()
+	{
+		Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+
+		Vector3[] normals = mesh.normals;
+		for (int i = 0; i < normals.Length; i++)
+		{
+			normals[i] = -1 * normals[i];
+		}
+
+		mesh.normals = normals;
+
+		for (int i = 0; i < mesh.subMeshCount; i++)
+		{
+			int[] tris = mesh.GetTriangles(i);
+			for (int j = 0; j < tris.Length; j+=3)
+			{
+				int temp = tris[j];
+				tris[j] = tris[j + 1];
+				tris[j + 1] = temp;
+			}
+
+			mesh.SetTriangles(tris, i);
+		}
 	}
 }
