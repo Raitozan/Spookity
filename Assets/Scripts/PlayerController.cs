7f6e743fc355281ffc94a02b03a80f059c3f2e37
Nathan Desages <nathan.desages@gmail.com>
2018-12-04 21:48:54 +0100

    added baseAttack

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,22 +12,24 @@ public class PlayerController : MonoBehaviour {
 	public GameObject leftLeg;
 	public GameObject rightLeg;
 
+	public Animator animator;
 
 	void Start()
     {
-      
-    }
+		animator.SetBool("baseAttackRight", true);
+	}
 
 
     void Update()
     {
         Move();
+		BaseAttack();
         LeftArm();
         RightArm();
         LeftLeg();
         RightLeg();
         Head();
-    }
+	}
 
     private void Move()
 	{
@@ -43,9 +45,35 @@ public class PlayerController : MonoBehaviour {
 			x = Input.GetAxis("Vertical2");
 		}
 
+		if (x == 0 && z == 0)
+		{
+			animator.SetBool("isMoving", false);
+		}
+		else
+		{
+			animator.SetBool("isMoving", true);
+		}
+
         //transform.Translate(x * speed * Time.deltaTime, 0, z * speed * Time.deltaTime);
 		GetComponent<Rigidbody>().MovePosition(transform.position + transform.forward * (x * speed * Time.deltaTime) + transform.right * (z * speed * Time.deltaTime));
     }
+
+	private void BaseAttack()
+	{
+		animator.SetBool("isAttacking", false);
+		if ((Input.GetButtonDown("BaseAttack") && CompareTag("Player1")) || (Input.GetButtonDown("BaseAttack2") && CompareTag("Player2")))
+		{
+			Debug.Log(Input.GetButtonDown("BaseAttack"));
+			Debug.Log(CompareTag("Player1"));
+			Debug.Log(Input.GetButtonDown("BaseAttack2"));
+			Debug.Log(CompareTag("Player2"));
+			if (leftArm.activeInHierarchy || rightArm.activeInHierarchy)
+			{
+				animator.SetBool("isAttacking", true);
+			}
+
+		}
+	}
 
     private void LeftArm()
     {
@@ -60,7 +88,8 @@ public class PlayerController : MonoBehaviour {
         if ((Input.GetButtonDown("RightArm") && CompareTag("Player1")) || (Input.GetButtonDown("RightArm2") && CompareTag("Player2")))
         {
             Debug.Log("Lancer bras droit");
-        }
+			animator.SetBool("baseAttackRight", false);
+		}
     }
 
     private void LeftLeg()
@@ -85,6 +114,5 @@ public class PlayerController : MonoBehaviour {
 		{
             Debug.Log("Lancer tête");
         }
-        
     }
 }
